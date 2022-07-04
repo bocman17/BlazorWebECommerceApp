42005633d67387a6610e161876fb02fc6b140030
Tomáš Hejlík <hejlikt@gmail.com>
2022-07-04 10:41:06 +0200

    Added good practices, services etc...

--- BlazorWebECommerceApp/Client/Program.cs
+++ BlazorWebECommerceApp/Client/Program.cs
@@ -1,3 +1,6 @@
+global using BlazorWebECommerceApp.Shared;
+global using System.Net.Http.Json;
+global using BlazorWebECommerceApp.Client.Services.ProductService;
 using BlazorWebECommerceApp.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,5 +10,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IProductService, ProductService>();
 
 await builder.Build().RunAsync();
